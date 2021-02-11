28b2b824579b586ac54cfb08b1aba057265a35bb
LucaRaveri <luca.raveri@yahoo.it>
2021-02-11 14:56:16 +0100

    methods definition

--- bst.h
+++ bst.h
@@ -22,8 +22,8 @@ class bst {
 
 public:
 
-	  using pair = std::pair<const Tk,Tv>;
-	  using node_type = node<pair>;
+	  using pair_type = std::pair<const Tk,Tv>;
+	  using node_type = node<pair_type>;
 	  using Iterator = iterator<node_type, typename node_type::value_type>;
 	  using Const_iterator = iterator<node_type, const typename node_type::value_type>;
 
@@ -39,29 +39,72 @@ private:
 
 	// TODO: copy semantics
 
+	bst(const bst& tree);
+
+	bst& operator=(const bst& tree);
+
 	// TODO: move semantics
+
+	bst(bst&& tree) noexcept;
+
+	bst& operator=(bst&&  tree) noexcept;
 
 	// TODO: subscription operator[]
 
-	// TODO: put to operator<<
+	Tv& operator[](const Tk& x);
 
-	// TODO: iterator begin and end
+	Tv& operator[](Tk&& x);
 
-	// TODO: const iterator begin and end
+	// TODO: put to operator<< !
 
-	// TODO: insert
+	friend
+	std::ostream& operator<<(std::ostream& os, const bst& x);
+
+	// TODO: iterator begin and end !
+
+	Iterator begin();
+
+	Iterator end();
+
+	// TODO: const iterator begin and end !
+
+	Const_iterator begin() const;
+
+	Const_iterator cbegin() const;
+
+	Const_iterator end() const;
+
+	Const_iterator cend() const;
+
+	// TODO: insert !
+
+	std::pair<Iterator, bool> insert(const pair_type& x);
+
+	std::pair<Iterator, bool> insert(pair_type&& x);
 
 	// TODO: emplace
 
-	// TODO: clear
+	template< class... Types >
+	std::pair<Iterator,bool> emplace(Types&&... args);
 
-	// TODO: find
+	// TODO: clear !
+
+	void clear();
+
+	// TODO: find !
+
+	Iterator find(const Tk& x);
+
+	Const_iterator find(const Tk& x) const;
 
 	// TODO: balance
 
+	void balance();
+
 	// TODO: erase
 
-	// TODO: print
+	void erase(const Tk& x);
+
 
 };
 
